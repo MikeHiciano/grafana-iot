9d18b369d49c3efc05b416b6b1f87e7aef5114e7
Miguel Hiciano <mike29hiciano@gmail.com>
2021-01-19 13:29:31 -0400

    [ADD] adding docker-compose, dockerfile and essential elements of the server

---
+++ dockerfile
@@ -0,0 +1,16 @@
+FROM ubuntu:16.04
+
+COPY ./server /usr/src/app/
+
+WORKDIR /usr/src/app/
+
+RUN apt-get update && \
+    apt-get install -y software-properties-common && \
+    add-apt-repository ppa:deadsnakes/ppa && \
+    apt-get -y update && \
+    apt-get install -y python3.7 && \
+    apt-get install -y python3-pip
+
+RUN pip3 install -r requirements.txt
+
+CMD ["python3","server.py"]
--- server/database.py
+++ server/database.py
@@ -1,19 +1,22 @@
 import psycopg2
 from datetime import date, datetime
 
-conn = psycopg2.connect("dbname= dht11 user=postgres host=localhost")
+conn = psycopg2.connect("dbname= dht11 user=mike host=localhost")
 
 def create_table():
     with conn:
         with conn.cursor() as cursor:
-            cursor.execute('CREATE TABLE IF NOT EXISTS measures (id SERIAL PRIMARY KEY,device varchar(45) NOT NULL, temperature integer NOT NULL, humidity integer NOT NULL, date date, hour integer);')
+            cursor.execute('CREATE TABLE IF NOT EXISTS measures (id SERIAL PRIMARY KEY,device varchar(45) NOT NULL, temperature integer NOT NULL, humidity integer NOT NULL, date date);')
 
 
 def insert_table(device,temperature,humidity):
     with conn:
         with conn.cursor() as cursor:
-            actual_date = date.today()
-            actual_time = datetime.now()
-            SQL = "INSERT INTO measures (device,temperature,humidity,date,hour) VALUES(%s,%s,%s,%s,%s) RETURNING True"
-            data = (device,temperature,humidity,actual_date,int(actual_time.hour))
+            SQL = "INSERT INTO measures (device,temperature,humidity,date,hour) VALUES(%s,%s,%s,(to_timestamp(%s, 'yyyy-mm-dd hh24:mi:ss'))) RETURNING True"
+            data = (device,temperature,humidity,datetime.now())
             cursor.execute(SQL, data)
+
+# def delete_table():
+#     with conn:
+#         with conn.cursor() as cursor:
+#             SQL = "DELETE measures WHERE "
