c42fcdf5a9ef9d9e0232039d5a5f52712923bc5d
Miguel Hiciano <mike29hiciano@gmail.com>
2021-01-31 13:37:03 -0400

    [UPD] adding wifi and mqtt funtionality on the microcontroller]

--- hardware/boot.py
+++ hardware/boot.py
@@ -1,9 +1,25 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 #import esp
 #esp.osdebug(None)
-import uos, machine
+import uos, time, gc, network
+from machine import Pin
 #uos.dupterm(None, 1) # disable REPL on UART(0)
-import gc
+
+led = Pin(2,Pin.OUT)
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+
+if wlan.isconnected() == False:
+    wlan.connect("<<ssid>>","<<pass>>")
+
+else:
+    for i in range(2):
+        led.on()
+        time.sleep_ms(300)
+        led.off()
+        time.sleep_ms(300)
+
 #import webrepl
 #webrepl.start()
 gc.collect()
--- hardware/main.py
+++ hardware/main.py
@@ -1,53 +1,81 @@
-import time , ds18x20 , onewire , ssd1306, gc
-from machine import Pin, ADC
+import time, ds18x20, onewire, ssd1306, dht, gc, ujson
+from machine import Pin, ADC, I2C
+from umqtt.simple import MQTTClient
+import gc
 
-i2c = machine.I2C(scl = Pin(5), sda = Pin(4))
+gc.collect()
+
+server = b"test.mosquitto.org"
+topic = b"sum_topic"
+
+# I2C OLED DISPLAY
+i2c = I2C(scl = Pin(5), sda = Pin(4))
 oled = ssd1306.SSD1306_I2C(128,64,i2c, 0x3c)
 oled.fill(0)
-
+# ADC,DS18B20 AND DHT11 SENSOR CODE
 adc = ADC(0)
 dat = Pin(14)
 ds = ds18x20.DS18X20(onewire.OneWire(dat))
-
+d = dht.DHT11(Pin(16))
 led = Pin(2,Pin.OUT)
-relay = Pin(16,Pin.OUT)
-
+#SCANNING THE ADDRESS OF THE DS18B20 SENSOR
 roms = ds.scan()
-print('found devices: %s' %(roms[0]))
+
+count = 0
+mqtt_count = 0
 
 def valmap(value, istart, istop, ostart, ostop):
-  return ostart + (ostop - ostart) * ((value - istart) / (istop - istart))
+    return ostart + (ostop - ostart) * ((value - istart) / (istop - istart))
 
-while True:
+def blink():
+    led.on()
+    time.sleep_ms(500)
+    led.off()
+    time.sleep_ms(500)
+
+def send_requests(humidity,relative_temp,absolupt_temp):
+    
+    values = {"humidity":humidity,"relative_temp":relative_temp,"absolupt_temp":absolupt_temp}
+    str_json = ujson.dumps(values)
+    c = MQTTClient(b"someone", server)
+    c.connect()
+    c.publish(topic,str_json)
+    c.disconnect()
+
+def show_measures():
+    
+    global count
+    global mqtt_count
     ds.convert_temp()
     analog_read = adc.read()
-    output = valmap(analog_read, 0,1024,-25,60)
-    time.sleep_ms(100)
+    output = valmap(analog_read, 0,1024,-25,60)  
+    count = count + 1
+    mqtt_count = mqtt_count + 1
 
-    if output >= ds.read_temp(roms[0]):
-        ds.convert_temp()
-        analog_read = adc.read()
-        oled.fill(0) 
-        oled.text("Temperature",0,0)
-        oled.text(str(ds.read_temp(roms[0])),0,10)
-        oled.text("analog",0,20)
-        oled.text(str(output),0,30)
-        oled.text("Gas leak, stoping machine",0,40)
-        oled.show()
-        time.sleep_ms(100)
-        oled.fill(0)
-        led.off()
-        relay.on()       
-        gc.collect()
+    if True:
+      oled.fill(0)
+      oled.text("Temp:",0,0)
+      oled.text(str(int(ds.read_temp(roms[0]))),40,0)
+      oled.text("analog:",0,10)
+      oled.text(str(int(output)),60,10)
+      oled.text("temp:",0,20)
+      oled.text(str(d.temperature()),40,20)
+      oled.text("hum:",0,30)
+      oled.text(str(d.humidity()),30,30)
+      oled.show()
+      time.sleep_ms(100)
+      oled.fill(0)
+      led.on()
+      gc.collect()
+    
+    if count == 10:
+      d.measure()
+      count = 0
 
-    else:
-        oled.text("Temperature",0,0)
-        oled.text(str(ds.read_temp(roms[0])),0,10)
-        oled.text("analog",0,20)
-        oled.text(str(output),0,30)
-        oled.show()
-        time.sleep_ms(100)
-        oled.fill(0)
-        led.on()
-        relay.off()
-        gc.collect()
+    if mqtt_count == 30:
+      send_requests(d.humidity(),d.temperature(),int(ds.read_temp(roms[0])))
+      mqtt_count = 0
+
+while True:
+  show_measures()
+
